b21c38032db879da91401bedf7d63d6000326d9f
Manu-Martinez <manu.martinezgalan@gmail.com>
2026-05-06 02:49:55 -0300

    deploying site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+	
+  site: 'https://github.io', 
+  
+  
+  base: '/Content-Creator-Portfolio', 
   devToolbar: {
     enabled: false,
   },
